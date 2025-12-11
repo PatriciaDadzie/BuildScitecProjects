5c7ee82b1859d0379f8f78d0b8fde66476c7dd8c
Patricia Dadzie <pdadzie502@gmail.com>
2025-12-11 14:04:52 +0000

    Add request validator middleware

---
+++ email-verification-backend/src/middleware/validator.js
@@ -0,0 +1,40 @@
+const Joi = require('joi');
+
+exports.validateEmail = (req, res, next) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required().messages({
+      'string.email': 'Please provide a valid email address',
+      'any.required': 'Email is required',
+    }),
+  });
+
+  const { error } = schema.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.details[0].message,
+    });
+  }
+
+  next();
+};
+
+exports.validateToken = (req, res, next) => {
+  const schema = Joi.object({
+    token: Joi.string().required().messages({
+      'any.required': 'Verification token is required',
+    }),
+  });
+
+  const { error } = schema.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.details[0].message,
+    });
+  }
+
+  next();
+};
